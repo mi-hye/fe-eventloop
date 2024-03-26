f8d3164b64c1c4db0dbb7a846dbf136863c136d0
mi-hye <satge13@gmail.com>
2024-03-26 22:04:15 +0900

    feat: Implement callstack

---
+++ src/js/components/CallStack.js
@@ -0,0 +1,29 @@
+import { ANIMATION } from "../utils/Constants.js";
+import Elements from "../utils/Elements.js";
+
+class CallStack {
+	#block;
+
+	constructor(code) {
+		this.#block = `<span class="code-box push">${code}</span>`;
+	}
+
+	push() {
+		Elements.$callStack.innerHTML = this.#block;
+		return new Promise((resolve) => setTimeout(() => resolve(), ANIMATION.stackDelay));
+	}
+
+	pop() {
+		const box = document.querySelector(".call-stack .code-box");
+		box.classList.remove("push");
+		box.classList.add("pop");
+		return new Promise((resolve) =>
+			setTimeout(() => {
+				box.remove();
+				resolve();
+			}, ANIMATION.stackDelay)
+		);
+	}
+}
+
+export default CallStack;
