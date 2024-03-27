a9bda6da1fe6b0c70d23dd93403cd79f8f5ae21d
mi-hye <satge13@gmail.com>
2024-03-27 13:49:02 +0900

    feat: Implement wepapi

--- src/js/utils/Elements.js
+++ src/js/utils/Elements.js
@@ -3,11 +3,13 @@ const Elements = {
 	$buttons: "",
 	$codeExcute: "",
 	$callStack: "",
+	$webAPI: "",
 	init() {
 		Elements.$buttons = document.querySelector(".code__input__btn");
 		Elements.$textArea = document.querySelector(".code__input > textarea");
 		Elements.$codeExcute = document.querySelector(".code__excute");
 		Elements.$callStack = document.querySelector(".call-stack > .box");
+		Elements.$webAPI = document.querySelector(".web-api > .box");
 	},
 };
 
