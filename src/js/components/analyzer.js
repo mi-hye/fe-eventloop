d3a2fae9782adcc191014f172c35289506eff9ca
mi-hye <satge13@gmail.com>
2024-03-28 15:40:26 +0900

    feat: Complite for cycle

--- src/js/components/analyzer.js
+++ src/js/components/analyzer.js
@@ -9,18 +9,20 @@ const memories = {
 	callstack: [],
 	microQueue: [],
 	taskQueue: [],
+	webApi: [],
 	async push(memory) {
 		memories[memory.toString()].push(memory);
 		await memory.push();
 	},
 	async pop(memory) {
+		const block = await memory.pop();
 		memories[memory.toString()].pop();
-		return await memory.pop();
+		return block;
 	},
-	async callStackPush(block) {
+	callStackPush(block) {
 		const callstack = new CallStack();
 		memories.callstack.push(callstack);
-		await callstack.eventloopPush(block);
+		callstack.eventloopPush(block);
 	},
 };
 
@@ -28,23 +30,33 @@ export async function analyze(code) {
 	//FIXME
 	const codeLines = Parser.getLines(code);
 
-	for (let i = 0; i < codeLines.length; i++) {
+	for (let i = 0; i <= codeLines.length; i++) {
 		let codeLine = codeLines[i];
 		const api = isWebAPI(codeLine);
 		if (api) codeLine = `${api}();`;
 
 		const callstack = new CallStack(codeLine);
-		await memories.push(callstack);
+		if (i < codeLines.length) await memories.push(callstack);
 
-		if (api) {
-			await webAPI(api, callstack, codeLines, i);
-		} //else await memories.pop(callstack);
+		if (api) await webAPI(api, callstack, codeLines, i);
+
+		if (i === codeLines.length) {
+			//아니면 task로
+			if (memories.webApi.length)
+				for (const memory of memories.webApi) {
+					const block = await memories.pop(memory);
+					const taskQueue = new TaskQueue(block);
+					await memories.push(taskQueue);
+				}
+		}
 
 		//eventroof가 callstack이 비어있으면 큐에 있는 애들 이동
 		await excuteEventLoop(memories);
 
 		if (memories.callstack.length)
-			memories.callstack.forEach(async (memory) => await memories.pop(memory));
+			for (const memory of memories.callstack) {
+				await memories.pop(memory);
+			}
 	}
 }
 
@@ -64,22 +76,13 @@ async function webAPI(api, callstack, codeLines, i) {
 	//wep이면
 	const callback = Parser.matchCallbackFn(codeLines[i]);
 	const webApi = new WebAPI(callback);
-	await webApi.push(); //wep으로 이동
+	await memories.push(webApi);
 	await memories.pop(callstack); // callstack 제거
 
 	if (webApi.isPromise(api)) {
 		//promise면 micro로
-		webApi.pop();
+		await memories.pop(webApi);
 		const micro = new MicroQueue(callback);
 		await memories.push(micro);
-		// await micro.push();
-	}
-
-	if (i === codeLines.length - 1) {
-		//아니면 task로
-		webApi.pop();
-		const taskQueue = new TaskQueue(callback);
-		await memories.push(taskQueue);
-		// await taskQueue.push();
 	}
 }
