4cb0edda637709e9e28f47a864d213ba062a4e83
mi-hye <satge13@gmail.com>
2024-03-28 22:06:59 +0900

    refactor: Seperate&Modify filter Function

--- src/js/components/Parser.js
+++ src/js/components/Parser.js
@@ -1,3 +1,5 @@
+import { PARSER } from "../utils/Constants.js";
+
 const Parser = {
 	getLines(code) {
 		const splitedCode = code.split("\n").filter((v) => v);
@@ -20,9 +22,38 @@ const Parser = {
 		}
 	},
 	matchCallbackFn(line) {
-		let callback = line.match(/\(\) => {.*}/)?.[0];
-		callback = line.match(/\(.*\)/)?.[0].slice(1, -1);
+		const callback = this.callbackFilter(line);
+		if (callback.length > PARSER.maxLength)
+			return this.fnExpressionFilter(callback) + "{Function body}";
 		return callback;
+	},
+	isWebAPI(line) {
+		const WEB_API_REGEXP = [
+			/setTimeout/,
+			/setInterval/,
+			/setImmediate/,
+			/Promise/,
+			/then/,
+			/fetch/,
+			/catch/,
+		];
+		const result = WEB_API_REGEXP.find((regexp) => regexp.test(line));
+		if (result) return result.toString().slice(PARSER.firstChar, PARSER.lastChar);
+		return false;
+	},
+	callbackFilter(line) {
+		const braces = line.match(/\(\) => {.*}/)?.join();
+		const parentheses = line
+			.match(/\(.*\)/)
+			?.join()
+			.slice(PARSER.firstChar, PARSER.lastChar);
+		const fnExpression = line.match(/function().+}/)?.join();
+		return braces || parentheses || fnExpression;
+	},
+	fnExpressionFilter(callback) {
+		const arrowFn = callback.match(/.+=>/)?.join();
+		const fnExpression = callback.match(/function\(\)/)?.join();
+		return arrowFn || fnExpression;
 	},
 };
 
