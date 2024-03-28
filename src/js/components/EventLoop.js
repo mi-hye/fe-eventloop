9db7cc19cfefab6782e26bca43cdf196d550b6fe
mi-hye <satge13@gmail.com>
2024-03-28 14:03:26 +0900

    feat: Add event loop->stack animation

--- src/js/components/EventLoop.js
+++ src/js/components/EventLoop.js
@@ -3,16 +3,22 @@ import Elements from "../utils/Elements.js";
 import CallStack from "./CallStack.js";
 
 function excuteEventLoop(memories) {
+	//TODO refactor
 	if (!memories.callstack.length && memories.microQueue.length) {
 		Elements.$eventLoop.classList.add("excute");
 		//스택이 비어있고 마이크로 큐가 비어있지 않으면 실행
-	} else {
+		memories.microQueue.forEach(async (memory) => {
+			const block = await memories.pop(memory);
+			await memories.callStackPush(block);
+		});
+	}
+
+	if (!memories.callstack.length && memories.taskQueue.length) {
+		//스택이 비어있고 테스크 큐가 비어있지 않으면 실행
 		Elements.$eventLoop.classList.add("excute");
-		let box;
 		memories.taskQueue.forEach(async (memory) => {
-			box = await memory.pop();
-			const callstack = new CallStack();
-			await callstack.eventloopPush(box);
+			const block = await memories.pop(memory);
+			await memories.callStackPush(block);
 		});
 	}
 
