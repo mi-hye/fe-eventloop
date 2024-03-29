d49297186893db5ff104ff77c7174bfad3258410
mi-hye <satge13@gmail.com>
2024-03-29 13:48:37 +0900

    refactor: Change WebAPI class to prototype

--- src/js/components/WebAPI.js
+++ src/js/components/WebAPI.js
@@ -1,34 +1,29 @@
 import { ANIMATION } from "../utils/Constants.js";
 import Elements from "./Elements.js";
 
-class WebAPI {
-	#block;
-	constructor(code) {
-		const $span = document.createElement("span");
-		$span.innerHTML = `${code}`;
-		$span.classList.add("code-box", "goto-web");
-		this.#block = $span;
-	}
-
-	push() {
-		Elements.$webAPI.prepend(this.#block);
-		return new Promise((resolve) => setTimeout(() => resolve(), ANIMATION.delay));
-	}
-
-	pop() {
-		const block = document.querySelector(".web-api .code-box");
-		setTimeout(() => block.remove());
-		return block.innerHTML;
-	}
-
-	isPromise(api) {
-		const WEP_API = ["Promise", "then", "catch"];
-		return WEP_API.includes(api);
-	}
-
-	toString() {
-		return "webApi";
-	}
+function WebAPI(code) {
+	const $span = document.createElement("span");
+	$span.innerHTML = `${code}`;
+	$span.classList.add("code-box", "goto-web");
+	this.block = $span;
 }
+
+WebAPI.prototype.push = () => {
+	Elements.$webAPI.prepend(this.block);
+	return new Promise((resolve) => setTimeout(() => resolve(), ANIMATION.delay));
+};
+
+WebAPI.prototype.pop = () => {
+	const block = document.querySelector(".web-api .code-box");
+	setTimeout(() => block.remove());
+	return block.innerHTML;
+};
+
+WebAPI.prototype.isPromise = (api) => {
+	const WEP_API = ["Promise", "then", "catch"];
+	return WEP_API.includes(api);
+};
+
+WebAPI.prototype.toString = () => "webApi";
 
 export default WebAPI;
